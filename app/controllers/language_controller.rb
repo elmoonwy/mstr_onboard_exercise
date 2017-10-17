0132b015d25fcce163483c9e6cdda9c2cf69a1b2
mawang <mawang@microstrategy.com>
2017-10-17 17:46:33 -0400

    adjust response

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -2,10 +2,7 @@ class LanguageController < ApplicationController
   def languages
     languages = ["swift", "java", "c", "python", "javascript", "html", "css", "ruby"].sample(rand(1..3))
     color = ["red", "yellow", "blue", "green"].sample
-    json_response = languages.map { |language| { title: language, color: color  } }
-    respond_to do |f|
-      f.html
-      f.json { render body: json_response }
-    end
+    json_response = languages.map { |language| { title: language, color: color } }
+    render json: json_response
   end
 end
